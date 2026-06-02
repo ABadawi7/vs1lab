fca45828a1175bde3a5a932542da770982336c4a
Abdulrahman Badawi <abadawi07@MacBook-Air-von-Abdulrahman.local>
2026-06-02 10:45:35 +0200

    Add DELETE route for GeoTags API

--- Aufgabe4/gta_v4/routes/index.js
+++ Aufgabe4/gta_v4/routes/index.js
@@ -216,5 +216,21 @@ router.put('/api/geotags/:id', (req, res) => {
  */
 
 // TODO: ... your code here ...
+router.delete('/api/geotags/:id', (req, res) => {
+  // Read id from URL path.
+  const id = req.params.id;
+
+  // Delete GeoTag from store.
+  const deletedGeoTag = geoTagStore.deleteGeoTag(id);
+
+  // Return 404 if GeoTag does not exist.
+  if (!deletedGeoTag) {
+    return res.status(404).json({ error: 'GeoTag not found' });
+  }
+
+  // Send deleted GeoTag as JSON.
+  res.json(deletedGeoTag);
+});
+
 
 module.exports = router;
